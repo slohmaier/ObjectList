0ce26d74762bfb27264afc4a692267c9afad470b
Stefan Lohmaier <stefan@slohmaier.de>
2022-11-06 11:17:57 +0100

    try fixing it

--- addon/globalPlugins/ObjectList.py
+++ addon/globalPlugins/ObjectList.py
@@ -7,20 +7,28 @@ import config
 import globalPluginHandler
 import gui
 import wx
+from scriptHandler import script
 from gui import SettingsPanel
 from logHandler import log
 
 distdir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'dist32')
 sys.path.append(distdir)
-fy, set_languages
 
 #make _() available
 addonHandler.initTranslation()
 
 #configuration for settings
 config.conf.spec["LanguageIdentification"] = {
-	'whitelist': 'string(default=\'\')'
+	#'whitelist': 'string(default=\'\')'
 }
+
+class ObjectList(wx.Dialog):
+	def __init__(self, parent, reverse=False):
+		# Translators: The title of the Specific Search dialog.
+		super(ObjectList, self).__init__(parent, title=_("Specific search"))
+
+		mainSizer = wx.BoxSizer(wx.VERTICAL)
+		sHelper = gui.guiHelper.BoxSizerHelper(self, orientation=wx.VERTICAL)
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 	def __init__(self):
@@ -28,6 +36,18 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 
 		#add settings to nvda
 		#gui.settingsDialogs.NVDASettingsDialog.categoryClasses.append(ObjectListSettings)
+
+	@script(
+		description=_("Show object list from desktol with filter and option to focues an object."),
+		gesture="kb:NVDA+F8"
+	)
+	def script_showList(self, gesture):
+		def run():
+			gui.mainFrame.prePopup()
+			d = ObjectList(gui.mainFrame, reverse=reverse)
+			d.ShowModal()
+			gui.mainFrame.postPopup()
+		wx.CallAfter(run)
 
 class ObjectListSettings(SettingsPanel):
 	title = 'ObjectList'
