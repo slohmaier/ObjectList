16274400a3230eb58c054205780cf5637773cbf8
Stefan Lohmaier <stefan@slohmaier.de>
2025-01-06 21:04:55 +0100

    fix buildvars

--- buildVars.py
+++ buildVars.py
@@ -27,7 +27,7 @@ addon_info = {
 	# version
 	"addon_version": "0.1",
 	# Author(s)
-	"addon_author": "Stefan Lohmaier <stefan@slohmaier.de",
+	"addon_author": "Stefan Lohmaier <stefan@slohmaier.de>",
 	# URL for the add-on documentation support
 	"addon_url": None,
 	# URL for the add-on repository where the source code can be found
@@ -37,7 +37,7 @@ addon_info = {
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
 	"addon_minimumNVDAVersion": '2022.1.0',
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": '2022.8.0',
+	"addon_lastTestedNVDAVersion": '2024.4.1',
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
