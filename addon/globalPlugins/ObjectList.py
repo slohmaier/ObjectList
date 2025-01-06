36ae02ea6a7d1ecc9672c17cbc6f7b4dee4ba59a
Stefan Lohmaier <stefan@slohmaier.de>
2025-01-06 16:34:18 +0100

    dialog generation works now.

--- addon/globalPlugins/ObjectList.py
+++ addon/globalPlugins/ObjectList.py
@@ -6,7 +6,7 @@ import NVDAObjects.IAccessible
 import addonHandler
 import config
 import globalPluginHandler
-import gui
+import ui
 import wx
 from scriptHandler import script
 from gui.settingsDialogs import SettingsPanel
@@ -23,14 +23,17 @@ config.conf.spec["ObjectList"] = {
 }
 
 def indexObject(parent: NVDAObjects.IAccessible.NVDAObject, indent='  '):
-	output = ''
+	output = []
 	for child in parent.children:
-		child.role
-		name = child.name
-		if not name is None:
-			#output commana separated: name, role, roleText, states
-			output += f'{indent}{name}, {child.role}, {child.roleText}, {child.states}\n'
-		output += indexObject(child, indent + '  ')
+		output.append([
+			indent,
+			str(child.name),
+			child.role.name,
+			str(child.states),
+			str(child.isFocusable),
+			str(child.isProtected),
+		])
+		output += indexObject(child, indent + '--')
 	return output
 
 def listObjects(obj: NVDAObjects.IAccessible.NVDAObject):
@@ -38,15 +41,17 @@ def listObjects(obj: NVDAObjects.IAccessible.NVDAObject):
 		obj = obj.parent
 
 	if obj is None:
-		return 'No WindowRoot found'
+		ui.message('No WindowRoot found')
+		return None
 	else:
-		return indexObject(obj).strip()
+		return indexObject(obj)
 
 class ObjectList(wx.Dialog):
 	def __init__(self, focusObject: NVDAObjects.IAccessible.NVDAObject, reverse=False):
 		# Translators: The title of the Specific Search dialog.
-		super(ObjectList, self).__init__(parent, title=_("Specific search"))
+		wx.Dialog.__init__(self, None, title=_("ObjectList"), style=wx.DEFAULT_DIALOG_STYLE | wx.RESIZE_BORDER)
 
+		ui.message('ObjectList dialog created')
 		self.focusObject = focusObject
 
 		#add one big scrollable label to the dialog with
@@ -55,49 +60,46 @@ class ObjectList(wx.Dialog):
 		self.label = wx.TextCtrl(self, style=wx.TE_MULTILINE | wx.TE_READONLY | wx.TE_RICH2)
 		self.sizer.Add(self.label, 1, wx.EXPAND)
 		self.refreshButton = wx.Button(self, label=_("Refresh"))
+		self.refreshButton.Bind(wx.EVT_BUTTON, self.refresh)
 		self.sizer.Add(self.refreshButton, 0, wx.EXPAND)
 		self.SetSizer(self.sizer)
 
-	def refresh(self):
-		self.label.SetValue(listObjects(self.focusObject))
+		self.refresh(None)	
+
+	def refresh(self, event):
+		table = listObjects(self.focusObject)
+		if table is None:
+			self.label.SetValue('No WindowRoot found')
+			return
+		#add header
+		table.insert(0, ['Name', 'Role', 'States', 'Focusable', 'Protected'])
+		#convert to string
+		text = ''
+		for row in table:
+			text += ', '.join(row) + '\n'
+
+		self.label.SetValue(text)
 
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
-	def __init__(self):
-		super().__init__()
+	scriptCategory = _("ObjectList")
 
-		#add settings to nvda
-		#gui.settingsDialogs.NVDASettingsDialog.categoryClasses.append(ObjectListSettings)
+	def __init__(self, *args, **kwargs):
+		global service
+		super(GlobalPlugin, self).__init__(*args, **kwargs)
 	
 	def show_objectlist(self):
+		ui.message('showing objectlist')
 		#show ObjectList dialog always on top
-		dlg = ObjectList(None)
-		dlg.ShowModal()
-		dlg.Destroy()
+		self.objectList = ObjectList(api.getFocusObject())
+		# add minimize and maximize buttons
+		self.objectList.ShowModal()
 
 	def script_show_objectlist(self, gesture):
-		wx.CallAfter(self.show_objectlist)
+		ui.message('showing objectlist wrappe')
+		self.show_objectlist()
 	script_show_objectlist.__doc__ = _("Show Objects in current window.")
 
 	__gestures = {
 		"kb:shift+NVDA+o": "show_objectlist",
 	}
-
-class ObjectListSettings(SettingsPanel):
-	title = 'ObjectList'
-	panelDescription = 'ObjectList like Mac'
-
-	def makeSettings(self, settingsSizer):
-		sHelper = gui.guiHelper.BoxSizerHelper(self, sizer=settingsSizer)
-		introItem = sHelper.addItem(wx.StaticText(self, label=self.panelDescription))
-	
-	def _loadSettings(self):
-		pass
-
-	def onSave(self):
-		pass
-	
-	def onPanelActivated(self):
-		self._loadSettings()
-		self.Show()
-	
