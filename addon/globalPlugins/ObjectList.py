63a0df2e9a2a27f348ea81143cb6d021fe958e34
Stefan Lohmaier <stefan@slohmaier.de>
2025-01-06 14:53:53 +0100

    indexing as string works. shortcut does not

--- addon/globalPlugins/ObjectList.py
+++ addon/globalPlugins/ObjectList.py
@@ -23,33 +23,44 @@ config.conf.spec["ObjectList"] = {
 }
 
 def indexObject(parent: NVDAObjects.IAccessible.NVDAObject, indent='  '):
+	output = ''
 	for child in parent.children:
+		child.role
 		name = child.name
 		if not name is None:
-			log.error(indent + name)
-		indexObject(child, indent + '  ')
+			#output commana separated: name, role, roleText, states
+			output += f'{indent}{name}, {child.role}, {child.roleText}, {child.states}\n'
+		output += indexObject(child, indent + '  ')
+	return output
 
-def listObjects():
-	obj = api.getFocusObject()
-	# If no object is found, get the
+def listObjects(obj: NVDAObjects.IAccessible.NVDAObject):
+	while obj is not None and type(obj.parent) is not NVDAObjects.IAccessible.WindowRoot:
+		obj = obj.parent
+
 	if obj is None:
-		obj = api.getTopWindow()
-
-    # If no object is found, return
-	if obj is None:
-		return
-
-	indexObject(obj)
+		return 'No WindowRoot found'
+	else:
+		return indexObject(obj).strip()
 
 class ObjectList(wx.Dialog):
-	def __init__(self, parent, reverse=False):
+	def __init__(self, focusObject: NVDAObjects.IAccessible.NVDAObject, reverse=False):
 		# Translators: The title of the Specific Search dialog.
 		super(ObjectList, self).__init__(parent, title=_("Specific search"))
 
-		listObjects()
+		self.focusObject = focusObject
 
-		mainSizer = wx.BoxSizer(wx.VERTICAL)
-		sHelper = gui.guiHelper.BoxSizerHelper(self, orientation=wx.VERTICAL)
+		#add one big scrollable label to the dialog with
+		# a button below to refresh. text in label is selectable
+		self.sizer = wx.BoxSizer(wx.VERTICAL)
+		self.label = wx.TextCtrl(self, style=wx.TE_MULTILINE | wx.TE_READONLY | wx.TE_RICH2)
+		self.sizer.Add(self.label, 1, wx.EXPAND)
+		self.refreshButton = wx.Button(self, label=_("Refresh"))
+		self.sizer.Add(self.refreshButton, 0, wx.EXPAND)
+		self.SetSizer(self.sizer)
+
+	def refresh(self):
+		self.label.SetValue(listObjects(self.focusObject))
+
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 	def __init__(self):
@@ -57,18 +68,20 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 
 		#add settings to nvda
 		#gui.settingsDialogs.NVDASettingsDialog.categoryClasses.append(ObjectListSettings)
+	
+	def show_objectlist(self):
+		#show ObjectList dialog always on top
+		dlg = ObjectList(None)
+		dlg.ShowModal()
+		dlg.Destroy()
 
-	@script(
-		description=_("Show object list from desktol with filter and option to focues an object."),
-		gesture="kb:NVDA+F8"
-	)
-	def script_showList(self, gesture):
-		def run():
-			gui.mainFrame.prePopup()
-			d = ObjectList(gui.mainFrame, reverse=reverse)
-			d.ShowModal()
-			gui.mainFrame.postPopup()
-		wx.CallAfter(run)
+	def script_show_objectlist(self, gesture):
+		wx.CallAfter(self.show_objectlist)
+	script_show_objectlist.__doc__ = _("Show Objects in current window.")
+
+	__gestures = {
+		"kb:shift+NVDA+o": "show_objectlist",
+	}
 
 class ObjectListSettings(SettingsPanel):
 	title = 'ObjectList'
@@ -87,3 +100,4 @@ class ObjectListSettings(SettingsPanel):
 	def onPanelActivated(self):
 		self._loadSettings()
 		self.Show()
+	
